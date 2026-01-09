d9ac6d2b07b4fb0f7530e50b3385935192c8c7d8
akulayeshwanth888 <yeshwanthsai888@gmail.com>
2026-01-09 23:25:11 +0530

    Fix JWT role in login token

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -32,7 +32,9 @@ router.post("/login", async (req, res) => {
 
   const token = jwt.sign(
     { id: user._id, role: user.role },
-    process.env.JWT_SECRET
+    process.env.JWT_SECRET,
+    {  expiresIn: "1d"
+    }
   );
 
   res.json({ token });
